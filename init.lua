4a0595e1a5a44f54f6582a0bafb066eecc8499a0
Zack Glenn <ZackGlenn05@gmail.com>
2023-04-01 16:18:58 -0600

    correct astrocommunity dev configuration, ron ft recognition

--- init.lua
+++ init.lua
@@ -1,15 +1,15 @@
 return {
   -- Configure AstroNvim updates
   updater = {
-    channel = "stable",    -- "stable" or "nightly"
-    version = "latest",    -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "main",       -- branch name (NIGHTLY ONLY)
-    commit = nil,          -- commit hash (NIGHTLY ONLY)
-    pin_plugins = nil,     -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false,  -- skip prompts about breaking changes
+    channel = "stable", -- "stable" or "nightly"
+    version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+    branch = "main", -- branch name (NIGHTLY ONLY)
+    commit = nil, -- commit hash (NIGHTLY ONLY)
+    pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
+    skip_prompts = false, -- skip prompts about breaking changes
     show_changelog = true, -- show the changelog after performing an update
-    auto_quit = false,     -- automatically quit the current session after a successful update
-    remotes = {            -- easily add new remotes to track
+    auto_quit = false, -- automatically quit the current session after a successful update
+    remotes = { -- easily add new remotes to track
       --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
       --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
@@ -27,13 +27,13 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true,     -- enable or disable format on save globally
+        enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
-          "lua",
+          -- "lua",
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
@@ -85,6 +85,7 @@ return {
         --     foo = "fooscript",
         keymap = "devicetree",
         typ = "typst",
+        ron = "ron",
       },
       --   filename = {
       --     ["Foofile"] = "fooscript",
--- plugins/community.lua
+++ plugins/community.lua
@@ -1,8 +1,9 @@
 return {
   -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
-  dev = true,
-  branch = "dev-codelldb",
+  {
+    "AstroNvim/astrocommunity",
+    dev = true,
+  },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
