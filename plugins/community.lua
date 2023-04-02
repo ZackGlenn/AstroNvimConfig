202da4fa0a56866d7c15b7740c1efad5a238eaee
Zack Glenn <ZackGlenn05@gmail.com>
2023-04-02 15:05:01 -0600

    use latest astrocommunity

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,9 @@
 return {
   -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
+  {
+    "AstroNvim/astrocommunity",
+    branch = "main",
+  },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
