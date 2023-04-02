3b7abc4d64fe15e47762b0fb875fee9aa52720bd
Zack Glenn <ZackGlenn05@gmail.com>
2023-04-01 18:08:16 -0600

    remove unecessary comment

--- plugins/community.lua
+++ plugins/community.lua
@@ -19,23 +19,4 @@ return {
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.rust" },
-  -- {
-  --   "rust-tools.nvim",
-  --   opts = function()
-  --     local extension_path = vim.fn.stdpath "data" .. "/mason/packages/codelldb/extension/"
-  --     local codelldb_path = extension_path .. "adapter/codelldb"
-  --     local liblldb_path = extension_path .. "lldb/lib/liblldb"
-  --
-  --     if vim.loop.os_uname().sysname == "Linux" then
-  --       liblldb_path = liblldb_path .. ".so"
-  --     else
-  --       liblldb_path = liblldb_path .. ".dylib"
-  --     end
-  --
-  --     return {
-  --       server = require("astronvim.utils.lsp").config "rust_analyzer",
-  --       dap = { adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path) },
-  --     }
-  --   end,
-  -- },
 }
